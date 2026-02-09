c590cbdf64c0776e50dc40aa7eab1f99cb803e45
Bintang Naufal <1bintangnaufal@gmail.com>
2026-02-09 19:27:04 +0700

    fix:auth

--- src/main/java/com/fangbuilt/lc_loan_system/features/auth/api/dto/LoginRequest.java
+++ src/main/java/com/fangbuilt/lc_loan_system/features/auth/api/dto/LoginRequest.java
@@ -1,15 +1,17 @@
 package com.fangbuilt.lc_loan_system.features.auth.api.dto;
 
-import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class LoginRequest {
     
-    @NotBlank(message = "Email is required")
-    @Email(message = "Email must be valid")
-    private String email;
+    @NotBlank(message = "Username is required")
+    @Size(min = 6, max = 30, message = "Username must be between 6 and 30 characters")
+    @Pattern(regexp = "^[a-zA-Z0-9_]*$", message = "Username can only contain alphanumeric characters and underscores")
+    private String username;
     
     @NotBlank(message = "Password is required")
     private String password;
